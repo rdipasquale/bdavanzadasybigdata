cbf7f29cea1e97164f01e709cae0a50041d5400f
Martingt <m.garcia.tejeda@gmail.com>
2020-04-23 20:07:51 -0300

    Recorrido de una linea

--- subtes/src/main/java/ar/edu/uca/bd/subtes/repositories/EstacionRepository.java
+++ subtes/src/main/java/ar/edu/uca/bd/subtes/repositories/EstacionRepository.java
@@ -1,12 +1,17 @@
 package ar.edu.uca.bd.subtes.repositories;
 
+import java.util.Set;
+
 import javax.transaction.Transactional;
 
 import org.springframework.data.repository.CrudRepository;
 
 import ar.edu.uca.bd.subtes.entities.Estacion;
+import ar.edu.uca.bd.subtes.entities.Linea;
 
 @Transactional
 public interface EstacionRepository extends CrudRepository<Estacion, Integer> {
+	
+	public Set<Estacion> findByLinea(Linea linea);
 
 }
--- subtes/src/main/java/ar/edu/uca/bd/subtes/services/SubteService.java
+++ subtes/src/main/java/ar/edu/uca/bd/subtes/services/SubteService.java
@@ -1,11 +1,16 @@
 package ar.edu.uca.bd.subtes.services;
 
+import java.util.ArrayList;
+import java.util.Set;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import ar.edu.uca.bd.subtes.entities.Ciudad;
+import ar.edu.uca.bd.subtes.entities.Estacion;
+import ar.edu.uca.bd.subtes.entities.Linea;
 import ar.edu.uca.bd.subtes.repositories.CiudadRepository;
 import ar.edu.uca.bd.subtes.repositories.EstacionRepository;
 import ar.edu.uca.bd.subtes.repositories.LineaRepository;
@@ -36,5 +41,33 @@ public class SubteService {
 	{
 		return ciudadRep.findAll();
 	}
+	
+//	public Iterable<Estacion> findAllEstacionesByLinea(Linea linea)
+//	{
+//		Estacion estacionActual = linea.getCabecera();
+//		ArrayList<Estacion> recorrido = new ArrayList<Estacion>();
+//		
+//		Set<Estacion> estacionesPosibles = estRep.findByLinea(linea);
+//		
+//		estacionesPosibles.remove(cabecera);	
+//		
+//		while (estacionActual.getConectaCon t)
+//		
+//		while(!estacionesPosibles.isEmpty())
+//		{
+//			recorrido.add(estacionActual);
+//			estacionActual.getConectaCon().forEach((Estacion estacionVecina)->{
+//				if(estacionVecina.getLinea)
+//			});
+//			
+//		}
+//		
+//		1)
+//			Estacion 1 -> Estacion 2 -> est...
+//		
+//		
+//		
+//		
+//	}
 	
 }
